a9ccd268621ca7b6d2046d516371bdb2a886f28f
LukeEom <eluke1028@gmail.com>
2023-02-18 20:57:05 +0900

    mapstruct DI&Test completed

--- demo/src/test/java/com/wor/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/wor/demo/DemoApplicationTests.java
@@ -1,15 +1,23 @@
 package com.wor.demo;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+
+import com.wor.demo.user.dto.UserDto;
+import com.wor.demo.user.service.UserService;
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Autowired
 	private ApplicationContext applicationContext;
+
+	@Autowired
+	private UserService userService;
 
 	@Test
 	public void contextLoads() throws Exception{
@@ -21,5 +29,15 @@ class DemoApplicationTests {
 			}
 		}
 	}
+	
+	@Test
+	public ArrayList<UserDto> userControllerTest() throws Exception{
+		ArrayList<UserDto> userAll = userService.getUserAll();
 
+        if (userAll == null){
+            System.out.println("회원정보 정보가 없습니다"); // exception handle required
+        }
+		System.out.println(userAll);
+        return userAll;
+	}
 }
