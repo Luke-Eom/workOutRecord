0a9b8988ff2cf24159b65531afc84305dbc934eb
LukeEom <eluke1028@gmail.com>
2023-03-01 19:56:41 +0900

    restart
    front first then check ctrler-service-repo
    * check how and what can I send from front

--- demo/src/main/java/com/wor/demo/record/controller/RecordController.java
+++ demo/src/main/java/com/wor/demo/record/controller/RecordController.java
@@ -20,19 +20,17 @@ public class RecordController {
     @Autowired
     private RecordService rcService;
 
-    @GetMapping("/userRecord")
-    public ArrayList<RecordResponseDto> recordAll(@RequestParam("userId") String userId ) throws Exception {
-        ArrayList<RecordResponseDto> recordAll = rcService.getRecord(userId);
+    // 조회 : 해당 날짜 기록 조회
+    // userId는 세션으로 받아오고, 선택한 date받아와서 record 조회
+    @GetMapping("/{userId}/{date}")
+    // getRecord with userId and date
+    // recordId 바로 front에서 불러올 수 있으면 recordId로
 
-        if(recordAll == null){
-            throw new Exception();
-        }
-        return recordAll;
-    }
-
-    @PostMapping("/recordInsert")
+    // 생성 : 
+    // userId, date 확인 != null 이면 실행 -> 해당 date에 user의 record가 이미 있는지 없는지 확인
+    @PostMapping("/create")   
     public RecordDto.RecordResponseDto create(RecordDto record) throws Exception {
-
+        //if()
         return rcService.createRecord(record);
 
     }
